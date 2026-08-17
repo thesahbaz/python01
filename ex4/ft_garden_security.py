d4ea3e6dbe2b5eeb4712f6841cfd2a5a914b972d
Muhammed Emin Akgul <muhakgul@k1m03s06.42istanbul.com.tr>
2026-08-17 18:20:00 +0300

    ex4 finish

--- ex4/ft_garden_security.py
+++ ex4/ft_garden_security.py
@@ -7,14 +7,20 @@ class Plant:
         self._age = age
         self.grow_rate = grow_rate
 
+    def show(self) -> None:
+        print(f"{self.name}: {self._height}cm, {self._age} days old")
+
     def grow(self) -> None:
         self._height = round(self._height + self.grow_rate, 1)
 
     def age(self) -> None:
         self._age += 1
 
+    def valid(self, value) -> bool:
+        return value >= 0
+
     def set_height(self, new_height) -> None:
-        if new_height >= 0:
+        if self.valid(new_height):
             self._height = new_height
             print(f"Height updated: {self._height}cm")
         else:
@@ -25,7 +31,7 @@ class Plant:
         return self._height
 
     def set_age(self, new_age) -> None:
-        if new_age >= 0:
+        if self.valid(new_age):
             self._age = new_age
             print(f"Age updated: {self._age} days")
         else:
@@ -34,21 +40,20 @@ class Plant:
 
     def get_age(self) -> float:
         return self._age
-    
-    def show(self) -> None:
-        print(f"{self.name}: {self._height}cm, {self._age} days old")
-
 
 def main():
     print("=== Garden Security System ===")
     lily = Plant("Lily", 15.0, 10, 0.8)
     print("Plant created: ", end="")
     lily.show()
+    print()
     lily.set_height(25)
     lily.set_age(30)
+    print()
     lily.set_height(-5)
     lily.set_age(-3)
-    print(f"Current state: ", end="")
+    print()
+    print("Current state: ", end="")
     lily.show()
 
 if __name__ == "__main__":
